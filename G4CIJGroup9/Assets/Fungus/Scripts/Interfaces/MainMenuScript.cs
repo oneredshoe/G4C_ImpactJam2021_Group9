bc748843f1af4f7b6c521b0f2c03ee0d96480560
Natacha-L <57923509+Natacha-L@users.noreply.github.com>
2021-03-24 22:47:51 +0100

    MainMenu scene

---
+++ G4CIJGroup9/Assets/Fungus/Scripts/Interfaces/MainMenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
